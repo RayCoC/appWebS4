d63ce0628dec01eb12e4fcd08817b30c6a3319fc
rrr <rayanhamok@yahoo.fr>
2022-02-27 15:20:48 +0100

    reglage header

--- my-app/src/header/Header.js
+++ my-app/src/header/Header.js
@@ -1,9 +1,24 @@
 import React, {useState, useEffect} from "react";
-import "./header.css"
+import "./header.css";
+import {Nav, Navbar} from "react-bootstrap";
 
 const Header = () => {
     return (
-        <header className="header">
+        <Navbar bg="dark" sticky="top" expand="lg" collapseOnSelect>
+            <Navbar.Brand>
+                <a className="navbar-brand" href="#" style={{color:"white"}}>iRayBuy</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Nav className='m-auto'>
+                    <Nav.Link className="link" href="Accueil">Accueil</Nav.Link>
+                    <Nav.Link className="link" href="Vendre">Vendre</Nav.Link>
+                    <Nav.Link className="link" href="Estimer">Estimer</Nav.Link>
+                    <Nav.Link className="link" href="Account">Mon compte</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+       /* <header className="header">
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container">
                 <a className="navbar-brand" href="#">iRayBuy</a>
@@ -25,7 +40,7 @@ const Header = () => {
                 </div>
             </div>
         </nav>
-    </header>
+    </header>*/
     )
 }
 export default Header;
