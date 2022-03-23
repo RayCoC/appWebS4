ca44fcfdb7f685dcd5af15699f4a146ab82826fa
RayCoC <83591085+RayCoC@users.noreply.github.com>
2022-03-23 10:17:11 +0100

    Update privateRoute.js

--- my-app/src/privateRoute.js
+++ my-app/src/privateRoute.js
@@ -3,11 +3,8 @@ import { Navigate, Route } from 'react-router-dom'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
 
-    // Add your own authentication on the below line.
     const isLogged = window.sessionStorage.getItem("userID") ? true : false;
 
-    // If authorized, return an outlet that will render child elements
-    // If not, return element that will navigate to login page
     return isLogged ? <Component/> : <Navigate to="/connexion" />;
 }
 
